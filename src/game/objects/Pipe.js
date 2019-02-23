54bac2be8aa0c7a25ee546ac8375ea99a4d08044
bergurijohansen <bergurij@gmail.com>
2019-02-23 17:25:55 +0000

    v1

--- src/game/objects/Pipe.js
+++ src/game/objects/Pipe.js
@@ -8,43 +8,69 @@ import "../../style/Frame.css";
 
 class Pipe {
   constructor() {
-    this.entity = new Entity(
+    this.entity1 = new Entity(
       "Pipe set 1",
       new Body(this, 1920 + 200, 1080 - 500, 800, 150),
       new Physics(this, -12.85, 0),
       new CollisionDetection(this)
     );
+
+    this.entity2 = new Entity(
+      "Pipe set 2",
+      new Body(this, 300, 1080 - 500, 800, 150),
+      new Physics(this, -12.85, 0),
+      new CollisionDetection(this)
+    );
+
+    this.entity = [this.entity1, this.entity2];
   }
 
   // entity method
   getCollisionDetection() {
-    return this.entity.getCollisionDetection();
+    return this.entity.map(index => {
+      return index.getCollisionDetection();
+    });
+    // return this.entity.getCollisionDetection();
   }
 
   // entity method
   getEntity() {
-    return this.entity;
+    // return this.entity.map(index => {
+    //   return index;
+    // });
+    return this.entity1;
   }
 
   // entity method
   getBody() {
-    return this.entity.getBody();
+    return this.entity.map(index => {
+      return index.getBody();
+    });
+    // return this.entity.getBody();
   }
 
   // entity method
   getPhysics() {
-    return this.entity.getPhysics();
+    return this.entity.map(index => {
+      return index.getPhysics();
+    });
+    // return this.entity.getPhysics();
   }
 
   // entity method
   update() {
-    this.entity.update();
+    this.entity1.update();
+    this.entity2.update();
   }
 
   // entity method
   getEntityProps() {
-    return this.entity.getEntityProps();
+    return this.entity[0].getEntityProps();
   }
+
+  // getEntityProps2() {
+  //   // return this.entity2.getEntityProps();
+  // }
 
   respawn = () => {
     let entity = Object.assign({}, this.entity); //creating copy of object
@@ -61,6 +87,7 @@ class Pipe {
 
   render() {
     let entityProps = this.getEntityProps();
+    // let entityProps2 = this.getEntityProps2();
     if (entityProps.bodyLeft < -150) {
       this.respawn();
     }
@@ -75,15 +102,15 @@ class Pipe {
       top: entityProps.bodyTop
     };
 
-    let divStyleTop = {
-      transform: "scaleY(-1)",
-      position: "absolute",
-      overflow: "hidden",
-      height: entityProps.bodyHeight,
-      width: entityProps.bodyWidth,
-      left: entityProps.bodyLeft,
-      top: entityProps.bodyTop - 280 - 800
-    };
+    // let divStyleTop = {
+    //   transform: "scaleY(-1)",
+    //   position: "absolute",
+    //   overflow: "hidden",
+    //   height: entityProps2.bodyHeight,
+    //   width: entityProps2.bodyWidth,
+    //   left: entityProps2.bodyLeft,
+    //   top: entityProps2.bodyTop - 280 - 800
+    // };
 
     const imgStyle = {
       overflow: "hidden",
@@ -94,9 +121,9 @@ class Pipe {
 
     return (
       <span className="frame">
-        <div style={divStyleTop}>
+        {/* <div style={divStyleTop}>
           <img src={PipeImg} style={imgStyle} />
-        </div>
+        </div> */}
         <div style={divStyle}>
           <img src={PipeImg} style={imgStyle} />
         </div>
