7f4389a4b313e9a982c96e0850160634331186b5
Bergur Johansen <bergurij@gmail.com>
2019-03-03 21:57:27 +0000

    riggar ikki enn

--- src/__tests__/AudioManager.test.js
+++ src/__tests__/AudioManager.test.js
@@ -1,4 +1,5 @@
 import AudioManager from "../gameEngine/components/AudioManager";
+import ResourceManager from "../utils/ResourceManager";
 describe("Audio Manager", () => {
   let a;
 
@@ -8,5 +9,30 @@ describe("Audio Manager", () => {
 
   it("bla", () => {
     expect(a).toMatchObject(new AudioManager(["123", "123", "123"]));
+  });
+
+  it("sooner", () => {
+    let bergur = new Audio(ResourceManager.getAudioPath("soundEffect2.mp3"));
+    if (bergur.duration > 0) {
+      console.log("pre 0");
+    }
+    bergur.play();
+    if (bergur.duration > 0) {
+      console.log("greater than 0");
+    } else {
+      console.log("hmmmmmm");
+    }
+
+    expect(
+      new Audio(ResourceManager.getAudioPath("soundEffect2.mp3")).play()
+    ).toBe(true);
+  });
+
+  it("later", () => {
+    let aa = new AudioManager(ResourceManager.getAudioPath("soundEffect1.mp3"));
+    aa.play();
+    let bob = new Audio(ResourceManager.getAudioPath("soundEffect2.mp3"));
+    console.log("bob", bob);
+    expect(bob.play()).toBe(!aa.paused);
   });
 });
