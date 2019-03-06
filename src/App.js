7e4eb80982dd5408fbaa45f062329d3f9d2fbc9a
Herborg <Herborg@LAPTOP-5O84QV7D>
2019-03-06 13:44:27 +0000

    scoreBox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
-//import Flappy from "./flappy/flappy";
-import Dino from "./dinosaur/dinosaur";
+import Flappy from "./flappy/flappy";
+//import Dino from "./dinosaur/dinosaur";
 class App extends Component {
   render() {
-    return <Dino />;
+    return <Flappy />;
   }
 }
 
